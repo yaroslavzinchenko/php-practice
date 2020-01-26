b46a992e848a04dd4dd77545622fd0f5eaeb0f83
Yaroslav Zinchenko <zin.yar@mail.ru>
2020-01-26 20:25:36 +0300

    Update index.php

--- php-experiments/test/index.php
+++ php-experiments/test/index.php
@@ -1,3 +1,17 @@
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+	<textarea name="textarea"></textarea>
+	<p><input type="submit" name="submit"></p>
+</form>
+
 <?php
-	echo intval(3.999);
+	if (!empty($_POST['submit']))
+	{
+		$file = 'test.txt';
+		$current .= "<!DOCTYPE html>
+		";
+		$current .= "<html>";
+		$current .= "<head>";
+		$current .= $_POST['textarea'];
+		file_put_contents($file, $current);
+	}
 ?>
